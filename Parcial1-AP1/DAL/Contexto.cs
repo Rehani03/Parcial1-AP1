340180bb8302eda600e0310c2e64703545fcc1f6
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2019-09-18 13:42:28 -0400

    Carpeta DAL programada

---
+++ Parcial1-AP1/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Parcial1_AP1.DAL
+{
+    public class Contexto : DbContext
+    {
+        //public DbSet<> Persona { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+
+    }
+}
