5ed963571f81e2d5205a68deb22925f171832c45
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2019-09-23 19:59:09 -0400

    Haciendo cambios en el test

--- Parcial1-AP1Tests/BLL/EvaluacionBLLTests.cs
+++ Parcial1-AP1Tests/BLL/EvaluacionBLLTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Parcial1_AP1.BLL;
+using Parcial1_AP1.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,17 @@ namespace Parcial1_AP1.BLL.Tests
         public void GuardarTest()
         {
 
-            Assert.Fail();
+            Evaluacion e = new Evaluacion();
+            e.IDEvaluacion1 = 0;
+            e.Estudiante1 = "Pedro";
+            e.Fecha = DateTime.Now;
+            e.Valor = 31;
+            e.Logadro = 20;
+            e.Perdido = 14;
+            e.Pronostico = 1;
+            bool paso = EvaluacionBLL.Guardar(e);
+            
+            Assert.AreEqual(paso, true);
         }
 
         [TestMethod()]
