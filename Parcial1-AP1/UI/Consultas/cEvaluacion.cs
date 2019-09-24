9beb86a43a1a8b839b91fbe52789ed743b832693
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2019-09-24 15:29:59 -0400

    Cambios en Consulta

--- Parcial1-AP1/UI/Consultas/cEvaluacion.cs
+++ Parcial1-AP1/UI/Consultas/cEvaluacion.cs
@@ -27,10 +27,10 @@ namespace Parcial1_AP1.UI.Consultas
             {
                 switch (FiltrocomboBox.SelectedIndex) 
                 {
-                    case 0:
+                    case 0: //Todo
                         lista = EvaluacionBLL.GetList(p => true);
                         break;
-                    case 1:
+                    case 1: //ID
                         try
                         {
                             int ID = Convert.ToInt32(CriteriotextBox.Text);
@@ -39,17 +39,18 @@ namespace Parcial1_AP1.UI.Consultas
                         catch (Exception)
                         {
 
-                            MessageBox.Show("El criterio para el ID debe ser numerico");
+                            MessageBox.Show("El criterio para el ID debe ser numérico.");
                         }
                         break;
-                    case 2:
+                    case 2: //Nombre o Estudiante
                         lista = EvaluacionBLL.GetList(p => p.Estudiante1.Contains(CriteriotextBox.Text));
                         break;
                     default:
-                        MessageBox.Show("Esta opción no existe");
+                        MessageBox.Show("Esta opción no existe.");
                         break;
                 }
 
+                //Fecha
                 lista = lista.Where(p => p.Fecha.Date >= DesdedateTimePicker.Value.Date
                 && p.Fecha.Date <= HastadateTimePicker.Value.Date).ToList();
 
